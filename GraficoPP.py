65bdf5a0a33215b29beceee67e78e7fa78613a31
Thubaralei <125900757+Thubaraleii@users.noreply.github.com>
2024-11-09 22:20:48 -0300

    Delete GraficoPP.py

--- GraficoPP.py
+++
@@ -1,9 +0,0 @@
-# Plotting without the title on top, as requested
-
-plt.figure(figsize=(6, 10))
-plt.plot(df_depth_variation['Número total de fósseis'], df_depth_variation['Profundidade Média (cm)'], marker='o', linestyle='-', color='orange')
-plt.xlabel('Número total de fósseis')
-plt.ylabel('Profundidade Média (cm)')
-plt.gca().invert_yaxis()  # Inverting Y-axis for depth visualization
-plt.grid(True)
-plt.show()
