5599ff5826dd1178dcf3e510dbd024dd1601dc9b
Thubaralei <125900757+Thubaraleii@users.noreply.github.com>
2024-11-16 01:19:01 -0300

    Update generate_plots.py

--- generate_plots.py
+++ generate_plots.py
@@ -22,13 +22,20 @@ data_table2 = {
     'Na2O': [None, None, 0.38, 0.30, 0.33, 0.35, 0.86, 0.7, 0.87, 1.41, 1.28, 1.24, 1.37],
     'TiO2': [None, None, 0.57, 0.55, 0.59, 0.61, 0.6, 0.61, 0.61, 0.62, 0.64, 0.61, 0.63]
 }
+data_table3 = {
+    'profundidades' = [10, 35, 45, 60, 68, 82, 86.5, 94.5, 100, 106, 110, 111.5, 114]
+    'valores_2010' = [0, 92, 30, 33, 51, 200, 57, 44, 20, 20, 6, 0, 0]
+    'valores_2011' = [0, 71, 100, 104, 166, 241, 113, 52, 40, 64, 45, 10, 0]
+    'valores_2016' = [13, 0, 57, 34, 47, 68, 22, 19, 31, 21, 4, 6, 0]
 
 df_table1 = pd.DataFrame(data_table1)
 df_table2 = pd.DataFrame(data_table2)
+df_table3 = pd.DataFrame(data_table3)
 
 # Columns to plot for each table
 table1_columns = ['MOA', 'TOC', 'TS', 'TN', 'U/Th']
 table2_columns = ['Al2O3', 'Fe2O3', 'Cr2O3', 'Na2O', 'TiO2']
+table3_columns = ['valores_2010', 'valores_2011', 'valores_2016']
 
 # Function to save Mod4 style plots
 def save_mod4_plots(df, columns, filename_prefix):
@@ -51,3 +58,4 @@ def save_mod4_plots(df, columns, filename_prefix):
 # Saving plots for both tables
 save_mod4_plots(df_table1, table1_columns, 'mod4_table1')
 save_mod4_plots(df_table2, table2_columns, 'mod4_table2')
+save_mod4_plots(df_table3, table3_columns, 'mod4_table2')
